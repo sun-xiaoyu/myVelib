a24c82bf4892db733ef6971a6f090bc084edce8c
ZhihaoLi1 <zhihao.li@student-cs.fr>
2019-03-17 17:44:50 +0100

    17:45

--- myVelib/src/myVelib/Answer.java
+++ myVelib/src/myVelib/Answer.java
@@ -24,10 +24,5 @@ public class Answer {
 		return totalDis;
 	}
 	
-	@Override
-	public String toString() {
-		return "Answer [startStation=" + startStation + ", endStation=" + endStation + ", totalTime=" + totalTime
-				+ ", totalDis=" + totalDis + "]";
-	}
-	
+
 }
