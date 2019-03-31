53b2f702e44dddb79d0104464d5441ca9bca6195
sun-xiaoyu <43569888+sun-xiaoyu@users.noreply.github.com>
2019-03-31 21:41:30 +0200

    test scenario bugs!!!!

--- myVelib/src/planning/Map.java
+++ myVelib/src/planning/Map.java
@@ -179,7 +179,7 @@ public class Map {
 			 }
 		}
 		
-		int alreadySetMecNum = mecTotalBicycleNum;
+		int alreadySetMecNum = 0;
 		while(alreadySetMecNum < mecTotalBicycleNum) {
 			int index = random.nextInt(stations.size());
 			 Station s = stations.get(index+1);
